885e416ac6300db59e52a772a74160172a7ba4d5
Stevenzhai <Stevenzwzhai@users.noreply.github.com>
2017-05-24 11:06:48 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,7 +2,7 @@
  * Created by Stevenzwzhai on 2017/2/28.
  */
 console.log('Script loaded!')
-var cacheStorageKey = 'm-pwa'
+var cacheVersion = 'm-pwa'
 
 var cacheList = [
 	'/',
@@ -11,49 +11,52 @@ var cacheList = [
 	"a.png"
 ]
 
-self.addEventListener('install', function(e) {
-	console.log('Cache event!')
-	e.waitUntil(
-		caches.open(cacheStorageKey).then(function(cache) {
-			console.log('Adding to Cache:', cacheList)
-			//如果是一些大文件或者不稳定文件可以选择不返回cache.addAll，小文件再返回
-			return cache.addAll(cacheList)
-		}).then(function() {
-			console.log('Skip waiting!')
-			//更新的时候，新的sw不必等待，而是可以直接接管当前页面。
-			return self.skipWaiting()
-		})
-	)
+//install
+
+self.addEventListener('install', (event) =>{
+    // 如果查到文件没有缓存，则会发送请求去获取，并且会带上 cache-bust 的 query string
+    console.log('install');
+    self.skipWaiting()
+    event.waitUntil(
+        caches.open(cacheVersion)
+            //箭头函数在这里将缓存返回
+            .then(cache => cache.addAll(cacheList))
+            .then(() => self.skipWaiting())
+    )
 })
 
-self.addEventListener('activate', function(e) {
-	console.log('Activate event')
-	e.waitUntil(
-		Promise.all(
-			caches.keys().then(cacheNames => {
-				return cacheNames.map(name => {
-					if (name !== cacheStorageKey) {
-		return caches.delete(name)
-	}
-})
-})
-	).then(() => {
-		console.log('Clients claims.')
-	return self.clients.claim()
-})
-	)
+//缓存捕获
+self.addEventListener('fetch', (event) => {
+    console.log('fetch', event.request.url);
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if(response){
+                    return response;
+                }
+                console.log(event.request, event.request.url);
+                return fetch(event.request);
+            })
+    )
 })
 
-self.addEventListener('fetch', function(e) {
-	// console.log('Fetch event:', e.request.url)
-	e.respondWith(
-		caches.match(e.request).then(function(response) {
-			if (response != null) {
-				console.log('Using cache for:', e.request.url)
-				return response
-			}
-			console.log('Fallback to fetch:', e.request.url)
-			return fetch(e.request.url)
-		})
-	)
+//update更新
+self.addEventListener('activate', (event) => {
+    console.log('update');
+    event.waitUntil(
+        //会
+  
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.filter(cachename => {
+                    if(cachename == cacheVersion){
+                        return caches.delete(cachename);
+                    }
+                })
+            ).then(() => {
+                return self.clients.claim()
+            })
+        })
+        
+    )
 })
